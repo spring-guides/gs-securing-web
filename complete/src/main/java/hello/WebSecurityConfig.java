b07d5c64b80f3552133fbfd4b7b333cff53bd391
Rob Winch <rwinch@vmware.com>
2014-01-23 15:58:55 -0600

    Fix Sample
    - Previously the application displayed sign out from initial which was a link rather than a form. Remove Sign Out from initial while displaying proper Sign Out setup
    - Use EnableWebMvcSecurity and remove explicit CSRF tokens
    - Display the current user
    - Provide error and success messages on the login page
    - Update the greeting.png

--- complete/src/main/java/hello/WebSecurityConfig.java
+++ complete/src/main/java/hello/WebSecurityConfig.java
@@ -3,11 +3,11 @@ package hello;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebMvcSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -17,7 +17,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
         http
             .formLogin()
-                .defaultSuccessUrl("/hello")
                 .loginPage("/login")
                 .permitAll()
                 .and()
@@ -26,8 +25,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder authManagerBuilder) throws Exception {
-        authManagerBuilder.inMemoryAuthentication()
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER");
     }
 }
