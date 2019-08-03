f207ec8c68bc882c8718a66fa630d3631feb9c21
ryota.sugawara <ryo.sugawara.1224@gmail.com>
2019-08-03 20:52:38 +0900

    動作検証できたので設定クラスの中身を削除。

--- initial/src/main/java/hello/MvcConfig.java
+++ initial/src/main/java/hello/MvcConfig.java
@@ -1,23 +1,17 @@
 package hello;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Deprecated
-@Configuration
-public class MvcConfig implements WebMvcConfigurer {
+/**
+ * @deprecated ViewContorollerのRequestMappingに変更。
+ * @see hello.app.controller.ViewController 
+ */
+public class MvcConfig {
 
-	@Deprecated
-    /**
-     * @deprecated ViewContorollerのRequestMappingに変更。
-     * @see hello.app.controller.ViewController 
-     */
-    public void addViewControllers(ViewControllerRegistry registry) {
+//    public void addViewControllers(ViewControllerRegistry registry) {
 //        registry.addViewController("/home").setViewName("home");
 //        registry.addViewController("/").setViewName("home");
 //        registry.addViewController("/hello").setViewName("hello");
 //        registry.addViewController("/login").setViewName("login");
-    }
+//    }
 
 }
