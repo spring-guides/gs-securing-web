9ae3eeaf0065665b65ff2e411f176daf9fb21a46
Jay Bryant <jbryant@pivotal.io>
2019-10-24 15:48:54 -0500

    Upgraded to Spring Boot 2.2
    I upgraded the build files to Spring Boot 2.2.0, which necessitated changing the tests to use JUnit 5 and replacing the Gradle wrappers.

--- complete/src/main/java/com/example/securingweb/SecuringWebApplication.java
+++ complete/src/main/java/com/example/securingweb/SecuringWebApplication.java
@@ -6,14 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SecuringWebApplication {
 
-<<<<<<< HEAD:complete/src/main/java/hello/Application.java
 	public static void main(String[] args) throws Throwable {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(SecuringWebApplication.class, args);
 	}
-=======
-    public static void main(String[] args) throws Throwable {
-        SpringApplication.run(SecuringWebApplication.class, args);
-    }
->>>>>>> Use the Spring Initializr:complete/src/main/java/com/example/securingweb/SecuringWebApplication.java
 
 }
