6f04e57369960be5fac9cd29e1f989f95697ee00
ByoungJoonIm <bj.max.lim@gmail.com>
2020-08-29 09:48:26 +0900

    Update MvcConfig.java

--- complete/src/main/java/com/example/securingweb/MvcConfig.java
+++ complete/src/main/java/com/example/securingweb/MvcConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
+	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/home").setViewName("home");
 		registry.addViewController("/").setViewName("home");
