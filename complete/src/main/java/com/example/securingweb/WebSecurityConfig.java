97b85a3ee26809b88bc610189e2fac4cbfd28132
stepbystep2 <770954908@qq.com>
2021-05-18 11:13:59 +0800

    'withDefaultPasswordEncoder()' is deprecated

--- complete/src/main/java/com/example/securingweb/WebSecurityConfig.java
+++ complete/src/main/java/com/example/securingweb/WebSecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -31,8 +33,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	@Override
 	public UserDetailsService userDetailsService() {
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 		UserDetails user =
-			 User.withDefaultPasswordEncoder()
+			 User.builder().passwordEncoder(encoder::encode)
 				.username("user")
 				.password("password")
 				.roles("USER")
