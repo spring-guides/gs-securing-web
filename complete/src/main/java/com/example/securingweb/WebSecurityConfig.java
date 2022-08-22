1a5a197864fe26977c8f036ee32e8492af8d55dc
Marcus Da Coregio <marcusdacoregio@gmail.com>
2022-08-22 09:33:49 -0300

    Update Spring Security's configuration

--- complete/src/main/java/com/example/securingweb/WebSecurityConfig.java
+++ complete/src/main/java/com/example/securingweb/WebSecurityConfig.java
@@ -4,32 +4,33 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+public class WebSecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-			.authorizeRequests()
+			.authorizeHttpRequests((requests) -> requests
 				.antMatchers("/", "/home").permitAll()
 				.anyRequest().authenticated()
-				.and()
-			.formLogin()
+			)
+			.formLogin((form) -> form
 				.loginPage("/login")
 				.permitAll()
-				.and()
-			.logout()
-				.permitAll();
+			)
+			.logout((logout) -> logout.permitAll());
+
+		return http.build();
 	}
 
 	@Bean
-	@Override
 	public UserDetailsService userDetailsService() {
 		UserDetails user =
 			 User.withDefaultPasswordEncoder()
