57429911b364580a91798948d606ccee83fc7b2e
harze-dev <hankatari@comcast.net>
2025-06-26 00:14:00 -0400

    Finished basic deployment

---
+++ initial/src/main/java/com/example/securingweb/WebSecurityConfig.java
@@ -0,0 +1,57 @@
+package com.example.securingweb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    /*
+     * SecurityFilterChain- defines which URL paths should be secured
+     * i.e. "/" and "/home"
+     * all other paths require auth.
+     * 
+     * If user successfully logs in
+     * then redirect to requested page e.g. "/hello"
+     */
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests((requests) -> requests
+				.requestMatchers("/", "/home").permitAll()
+				.anyRequest().authenticated()
+			)
+			.formLogin((form) -> form
+				.loginPage("/login")
+				.permitAll()
+			)
+			.logout((logout) -> logout.permitAll());
+
+		return http.build();
+	}
+
+    /*
+     * UserDetailsService- sets up an in-memory user store (singe user)
+     * User is given user name and password, and role
+     * e.g. user, password, USER
+     */
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("password")
+				.roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
+}
