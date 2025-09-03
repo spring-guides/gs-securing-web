85705210842a6e8927a08bb07cd4b725c974d9b0
Danil.Pavlov <danil.pavlov@jetbrains.com>
2025-09-03 19:18:31 +0200

    Add Kotlin projects
    Signed-off-by: Danil Pavlov <danil.pavlov@jetbrains.com>

---
+++ complete-kotlin/src/main/kotlin/com/example/securingweb/SecuringWebApplication.kt
@@ -0,0 +1,11 @@
+package com.example.securingweb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecuringWebApplication
+
+fun main(args: Array<String>) {
+    runApplication<SecuringWebApplication>(*args)
+}
