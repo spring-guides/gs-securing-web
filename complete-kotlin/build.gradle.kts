9b9b14c2ee2a03e90d415d7f1e60c18e9e24ff82
Danil.Pavlov <danil.pavlov@jetbrains.com>
2025-09-16 14:55:38 +0200

    chore: Spring 3.5.5 and languages in README
    Signed-off-by: Danil Pavlov <danil.pavlov@jetbrains.com>

--- complete-kotlin/build.gradle.kts
+++ complete-kotlin/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.5.4"
+    id("org.springframework.boot") version "3.5.5"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
